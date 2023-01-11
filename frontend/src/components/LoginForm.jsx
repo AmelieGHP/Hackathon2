ee50f1ed4e485490155903ac5793c59a84a881b1
AMELIE GUILLOUX <amelie.guilloux@hotmail.fr>
2023-01-11 14:58:14 +0100

    created front structure with sass

---
+++ frontend/src/components/LoginForm.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+// style in loginPage.scss
+
+function LoginForm() {
+  return (
+    <div className="loginForm">
+      <p>Login form</p>
+    </div>
+  );
+}
+
+export default LoginForm;

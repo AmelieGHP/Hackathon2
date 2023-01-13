32f0372da972c23c9a459d7526bbf865a521282e
NaoSan9 <naomi.santos@outlook.fr>
2023-01-13 09:49:45 +0100

    Dashboard fixed

--- frontend/src/components/ListUser.jsx
+++ frontend/src/components/ListUser.jsx
@@ -3,36 +3,31 @@ import React from "react";
 function ListUser() {
   return (
     <div className="listuser">
-      <h3>Our team</h3>
+      <h4>Our team</h4>
       <div className="listEmp">
         <img
-          src={`${
-            import.meta.env.VITE_BACKEND_URL
-          }/assets/employees/black-mount-employee-image-1.jpg`}
+          src={`${import.meta.env.VITE_BACKEND_URL
+            }/assets/employees/black-mount-employee-image-1.jpg`}
           alt="utilisateur1"
         />
         <img
-          src={`${
-            import.meta.env.VITE_BACKEND_URL
-          }/assets/employees/black-mount-employee-image-2.jpg`}
+          src={`${import.meta.env.VITE_BACKEND_URL
+            }/assets/employees/black-mount-employee-image-2.jpg`}
           alt="utilisateur2"
         />
         <img
-          src={`${
-            import.meta.env.VITE_BACKEND_URL
-          }/assets/employees/black-mount-employee-image-3.jpg`}
+          src={`${import.meta.env.VITE_BACKEND_URL
+            }/assets/employees/black-mount-employee-image-3.jpg`}
           alt="utilisateur3"
         />
         <img
-          src={`${
-            import.meta.env.VITE_BACKEND_URL
-          }/assets/employees/black-mount-employee-image-4.jpg`}
+          src={`${import.meta.env.VITE_BACKEND_URL
+            }/assets/employees/black-mount-employee-image-4.jpg`}
           alt="utilisateur4"
         />
         <img
-          src={`${
-            import.meta.env.VITE_BACKEND_URL
-          }/assets/employees/black-mount-employee-image-5.jpg`}
+          src={`${import.meta.env.VITE_BACKEND_URL
+            }/assets/employees/black-mount-employee-image-5.jpg`}
           alt="utilisateur5"
         />
       </div>
